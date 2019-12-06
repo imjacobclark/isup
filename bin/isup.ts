eefdea3cf0abfffed11009e986bbafcd2cd9663e
Jacob Clark <jacob.clark@bbc.co.uk>
2019-12-05 17:36:09 -0800

    .gitignore

--- bin/isup.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import cdk = require('@aws-cdk/core');
-import { IsupStack } from '../lib/isup-stack';
-
-const app = new cdk.App();
-new IsupStack(app, 'IsupStack');
